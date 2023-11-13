ed76381bf4a8ddc7b071167af0b88db0884f59c5
Khawaritzmi Abdallah Ahmad <khawaritzmi@gmail.com>
2023-11-13 10:18:25 +0900

    major update 3.0

---
+++ script.js
@@ -0,0 +1,25 @@
+// script.js
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+let currentIndex = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${100 * (i - index)}%)`;
+    });
+}
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+});
+
+showSlide(currentIndex);
